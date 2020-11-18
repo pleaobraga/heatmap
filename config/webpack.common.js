a8760f09ded87c9f75cad3a899c370c87a92605a
pleaobraga <pleao.braga@gmail.com>
2020-11-18 08:03:07 -0300

    Add .env file

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,10 +2,11 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: './src/index.js'
   },
 
   plugins: [
@@ -13,16 +14,17 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'React Scaffold',
       template: 'public/index.html',
-      favicon: './images/react.png',
+      favicon: './images/react.png'
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new Dotenv()
   ],
 
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/'
   },
 
   module: {
@@ -37,13 +39,13 @@ module.exports = {
               [
                 '@babel/preset-react',
                 {
-                  throwIfNamespace: false,
-                },
+                  throwIfNamespace: false
+                }
               ],
-              '@babel/preset-env',
-            ],
-          },
-        },
+              '@babel/preset-env'
+            ]
+          }
+        }
       },
       {
         test: /\.scss$/,
@@ -51,17 +53,17 @@ module.exports = {
           'style-loader', // creates style nodes from JS strings
           'css-loader', // translates CSS into CommonJS
           'postcss-loader',
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
-        ],
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-    ],
+        use: ['file-loader']
+      }
+    ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx']
   },
   optimization: {
     splitChunks: {
@@ -69,9 +71,9 @@ module.exports = {
         vendor: {
           test: /[\\/]node_modules[\\/](react|react-dom|react-redux|react-router-dom|redux|redux-thunk|prop-types)[\\/]/,
           name: 'vendor',
-          chunks: 'all',
-        },
-      },
-    },
-  },
+          chunks: 'all'
+        }
+      }
+    }
+  }
 }
