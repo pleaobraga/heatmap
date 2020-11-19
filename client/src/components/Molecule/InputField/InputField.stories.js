440582a25c682bb265a2a029c250523fbd453a6e
pleaobraga <pleao.braga@gmail.com>
2020-11-19 10:38:42 -0300

    Remove blank line

--- client/src/components/Molecule/InputField/InputField.stories.js
+++ client/src/components/Molecule/InputField/InputField.stories.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
-
 import { InputField } from '.'
 
 export default storiesOf('Components | Molecule/InputField', module)
