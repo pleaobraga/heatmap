c78300350aa1f6e9a84b3a8194c9f2e56bded804
pleaobraga <pleao.braga@gmail.com>
2020-11-19 08:39:02 -0300

    Fix typo

--- client/src/pages/HeatmapPage/HeatmapPage.jsx
+++ client/src/pages/HeatmapPage/HeatmapPage.jsx
@@ -73,7 +73,7 @@ const HeatmapPage = () => {
             <LinkButton
               className="page-heatmap__form-button"
               href="/#submit"
-              handleCLick={toggleShowForm}
+              handleClick={toggleShowForm}
               text={showForm ? 'Esconder Formulário' : 'Adicionar Dados'}
             />
           )}
