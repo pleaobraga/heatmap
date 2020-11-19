05e384a733aa97c9f45d169874e7f8a9abbf8011
pleaobraga <pleao.braga@gmail.com>
2020-11-19 08:37:04 -0300

    Update InputField stories with error state

--- client/src/components/Molecule/InputField/InputField.stories.js
+++ client/src/components/Molecule/InputField/InputField.stories.js
@@ -4,83 +4,73 @@ import { text } from '@storybook/addon-knobs'
 
 import { InputField } from '.'
 
-export default storiesOf('Components | Molecule/InputField', module).add(
-  'default',
-  () => (
-    <div className="container" style={{ textAlign: 'inherit' }}>
-      <InputField
-        className="item"
-        label={text('Label', 'Label')}
-        onChange={() => {}}
-        name={text('Name', 'Name')}
-        error={{}}
-      />
-    </div>
-  ),
-  {
-    info: {
-      inline: true,
-      header: false,
-      source: false,
-      propTables: [InputField],
-      text: `
+export default storiesOf('Components | Molecule/InputField', module)
+  .add(
+    'default',
+    () => (
+      <div className="container" style={{ textAlign: 'inherit' }}>
+        <InputField
+          className="item"
+          label={text('Label', 'Label')}
+          onChange={() => {}}
+          name={text('Name', 'Name')}
+          error={{}}
+        />
+      </div>
+    ),
+    {
+      info: {
+        inline: true,
+        header: false,
+        source: false,
+        propTables: [InputField],
+        text: `
           ~~~js
           <InputField 
-            className="" 
-            text={text}
+            className=""
+            label={Label}
+            onChange={onChange}
+            name={name}
+            error={{}}
           />
           ~~~
       `
+      }
     }
-  }
-)
-// .add(
-//   'success',
-//   () => (
-//     <div className="container">
-//       <TypeMessage text={text('Text', 'Success text')} type="success" />
-//     </div>
-//   ),
-//   {
-//     info: {
-//       inline: true,
-//       header: false,
-//       source: false,
-//       propTables: [TypeMessage],
-//       text: `
-//         ~~~js
-//         <TypeMessage
-//           className=""
-//           text={text}
-//           type="success"
-//         />
-//         ~~~
-//     `
-//     }
-//   }
-// )
-// .add(
-//   'error',
-//   () => (
-//     <div className="container">
-//       <TypeMessage text={text('Text', 'Error text')} type="error" />
-//     </div>
-//   ),
-//   {
-//     info: {
-//       inline: true,
-//       header: false,
-//       source: false,
-//       propTables: [TypeMessage],
-//       text: `
-//         ~~~js
-//         <TypeMessage
-//           className=""
-//           text={text}
-//           type="error"
-//         />
-//         ~~~
-//     `
-//     }
-//   }
-// )
+  )
+  .add(
+    'error',
+    () => (
+      <div className="container" style={{ textAlign: 'inherit' }}>
+        <InputField
+          className="item"
+          label={text('Label', 'Label')}
+          onChange={() => {}}
+          name={text('Name', 'Name')}
+          error={{
+            hasError: true,
+            message: text('Error Message', 'Error Message')
+          }}
+        />
+      </div>
+    ),
+    {
+      info: {
+        inline: true,
+        header: false,
+        source: false,
+        propTables: [InputField],
+        text: `
+          ~~~js
+          <InputField 
+            className=""
+            label={Label}
+            onChange={onChange}
+            name={name}
+            error={error}
+          />
+          ~~~
+      `
+      }
+    }
+  )
